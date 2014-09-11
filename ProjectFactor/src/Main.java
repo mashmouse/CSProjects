5bbb5a42542c314cf20143b60886afa08e37ca00
mashmouse <marlimous3>
2014-09-11 09:44:09 -0700

    Cleaned up main. Next add more tests for the new methods, out all of the test methods in the same class.

--- ProjectFactor/src/Main.java
+++ ProjectFactor/src/Main.java
@@ -7,66 +7,68 @@ public class Main {
     	 System.out.println("Input a number inbetween 1 and 100 to get their factors, if you are finished you may type 'QUIT' to exit the program.");
     	 Scanner c = new Scanner(System.in);
          boolean quit = false;
-          while(quit == false){
+         while(quit == false){
         	  System.out.print("Input: ");
         	  String in = c.nextLine();
-        	  if(in.equals("QUIT")){
+        	  if(in.equalsIgnoreCase("QUIT")){
         		  quit = true;
         		  System.out.println("Thank you.");
         	  } else {
         		  try {
-        			  Scanner readLine = new Scanner(in);
-        			  int first = Integer.parseInt(readLine.next());
-        			  ArrayList<Integer> inputList = new ArrayList<Integer>();
-        			  inputList.add(first);
-        			  while(readLine.hasNext()){
-        				  inputList.add(Integer.parseInt(readLine.next()));
-        			  }
+        			  ArrayList<Integer> inputList = setUpInputList(in);
         			  if(inputList.size() == 1){
-        				  int input = inputList.get(0);
-        				  if(input <= 100 && input >= 1){
-            				  System.out.print("Factors: ");
-                	          ArrayList<Integer> listOfFactors = factors(input);
-                	          for(int j = 0; j < listOfFactors.size(); j++){
-                	        	  System.out.print(listOfFactors.get(j) + " ");
-                	          }
-            	        	  System.out.println();
+        				  if(inbetweenOneAndOneHundred(inputList) == true){
+        					  factors(inputList.get(0));
             			  } else {
             				  System.out.println("The number you input is not inbetween 1 and 100.");
             			  }  
         			  } else {
-        				  System.out.print("GCD: ");
-        				  System.out.println(getGCD(inputList));
+        				  if(inbetweenOneAndOneHundred(inputList) == true){
+        					  getGCD(inputList);
+        				  } else {
+        					  System.out.println("The numbers you input are not inbetween 1 and 100.");
+        				  }
         			  }
         		  } catch(NumberFormatException e) {
         			  System.out.println("Only input numbers please.");
         		  }
         	  }
           }
+          c.close();
+     }
+     
+     public static ArrayList<Integer> setUpInputList(String line){
+		  Scanner readLine = new Scanner(line);
+		  int first = Integer.parseInt(readLine.next());
+		  ArrayList<Integer> listInput = new ArrayList<Integer>();
+		  listInput.add(first);
+		  while(readLine.hasNext()){
+			  listInput.add(Integer.parseInt(readLine.next()));
+		  }
+		  readLine.close();
+		  return listInput;
      }
      
      public static ArrayList<Integer> factors(int num){
     	 ArrayList<Integer> factors = new ArrayList<Integer>();
+    	 System.out.print("Factors: ");
     	 for(int i = 1; i <= num; i++){
     		 for(int k = 1; k <= num; k++){
     			 if(i * k == num){
+    				 System.out.print(i + " ");
     				 factors.add(i);
     			 }
     		 }
     	 }
+    	 System.out.println();
     	 return factors;
      }
      
      public static int getGCD(ArrayList<Integer> nums){
-    	 int smallest = nums.get(0);
-    	 for(int i = 1; i < nums.size(); i++){
-        	 if(nums.get(i) < smallest){
-        		 smallest = nums.get(i);
-        	 }
-    	 }
+    	 int count = getSmallest(nums);
     	 boolean[] numsTest = new boolean[nums.size()];
-    	 int count = smallest;
-    	 while(smallest != 0){
+    	 System.out.print("GCD: ");
+    	 while(count != 0){
     		 for(int j = 0; j < numsTest.length; j++){
     			 if(nums.get(j)%count == 0){
     				 numsTest[j] = true;
@@ -76,6 +78,7 @@ public class Main {
     		 }
     		 for(int k = 0; k < numsTest.length; k++){
     			 if( k == numsTest.length - 1 && numsTest[k] == true){
+    				 System.out.println(count);
     				 return count; 
     			 } else if (numsTest[k] == false){
     				 count --;
@@ -84,6 +87,25 @@ public class Main {
     		 }
     	 }
     	 return 1;
+     }
+     
+     public static int getSmallest(ArrayList<Integer> numbers){
+    	 int smallest = numbers.get(0);
+    	 for(int i = 1; i < numbers.size(); i++){
+        	 if(numbers.get(i) < smallest){
+        		 smallest = numbers.get(i);
+        	 }
+    	 }
+    	 return smallest;
+     }
+     
+     public static boolean inbetweenOneAndOneHundred(ArrayList<Integer> listInput){
+    	 for(int k = 0; k < listInput.size(); k++){
+    		 if(listInput.get(k) > 100 || listInput.get(k) < 1){
+    			 return false;
+    		 }
+    	 }
+    	 return true;
      }
 
 }
